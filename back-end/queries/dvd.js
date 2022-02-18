8d7c155a02e106efb39501013713d5235ea0cb54
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-18 14:54:19 -0500

    function query by id done!

--- back-end/queries/dvd.js
+++ back-end/queries/dvd.js
@@ -3,13 +3,29 @@ const database = require('../db/dbConfig');
 const getAllDvds = async () => {
     try {
         const allDvds = await database.any(`
-            SELECT * FROM dvds_tb;
+            SELECT * FROM dvd_tb;
         `);
         return allDvds;
     } catch(error) {
         return error;
     };
 };
+
+const getOneDvd = async (id) => {
+    try {
+        const oneDvd = await database.one(`
+            SELECT * FROM dvd_tb WHERE id=$1
+        `, id);
+        return oneDvd;
+    } catch (error) {
+        return error;
+    };
+};
+
+
+
+
+
 
 module.exports = {
     getAllDvds,
