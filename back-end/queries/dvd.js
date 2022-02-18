013b24a38fe73951b0d7f342fb8f030738cd341c
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-18 15:48:16 -0500

    create query complete, queries feature is done

--- back-end/queries/dvd.js
+++ back-end/queries/dvd.js
@@ -54,6 +54,20 @@ const updateDvd = async ({ title, image, release_date, runtime, director, is_fav
     };
 };
 
+const createDvd = async ({ title, image, release_date, runtime, director, is_favorite }) => {
+    try {
+        const createdDvd = await database.one(`
+            INSERT INTO dvd_tb
+                (title, image, release_date, runtime, director, is_favorite)
+            VALUES
+                ($1, $2, $3, $4, $5, $6) 
+            RETURNING *
+        `, [title, image, release_date, runtime, director, is_favorite]);
+        return createdDvd;
+    } catch(error) {
+        return error;
+    };
+};
 
 module.exports = {
     getAllDvds,
