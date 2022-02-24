b8f9da4e79596a90265f167952d686aa3e8c5d5e
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-24 11:29:15 -0500

    created a home page

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,3 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Dvd from '../Components/Dvd';
+
 export default function Home() {
-    return <h2>Home Page</h2>
+    const [dvds, setDvds] = useState([]);
+    const API = process.env.REACT_APP_API_URL;
+
+    useEffect(() => {
+        axios
+            .get(`${API}/dvds`)
+            .then((response) => setDvds(response.data.payload))
+            .catch((error) => console.warn(error));
+    }, [API]);
+
+    return (
+        <div>
+            <h1>Featured Products</h1>
+            <br />
+            <hr />
+            <br />
+            <div className="home">
+                {dvds.map((dvd) =>  dvd.featured === true && <Dvd dvd={dvd} key={dvd.id} /> )}
+            </div>
+        </div>
+    );
 };
